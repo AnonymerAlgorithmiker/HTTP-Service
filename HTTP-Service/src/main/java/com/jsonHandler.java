abe1628d1d715bdfa9d602c2e8171fd537116ec3
AnonymerAlgorithmiker <linus_diepold@web.de>
2025-01-17 19:40:34 +0100

    Implemented Hello World and started with the JsonHandler

---
+++ HTTP-Service/src/main/java/com/jsonHandler.java
@@ -0,0 +1,19 @@
+package com;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class jsonHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException
+    {
+        String response = "Hello World";
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
